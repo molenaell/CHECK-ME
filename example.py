00f20825660a028797ea6d30cd20e8014c22ef81
molenaell <106754701+molenaell@users.noreply.github.com>
2022-08-02 13:26:38 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,10 +1,13 @@
+# open file , creating another one and writing the chosen in first file info into the other file
 f = open('roses.txt', "a", encoding='utf-8') # the file contains list of roses types
-f.write ("\nOld garden roses\n")
-f.write ("Modern garden roses\n")
-f.close()
+my_file = open("roses.update.txt", "w+")
+
 
 with open('roses.txt') as f: # converting each line into a list and picking the necessary element
     lines = f.readlines()
     e = lines[0]
 print(e)
+my_file.write (e)
 
+f.close()
+my_file.close()
